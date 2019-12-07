357dc02c7ad4030e607266be211c7b805991b1ec
Stephen Maitland <s.maitland@me.com>
2019-12-07 15:47:53 +0000

    42 Base Server

--- server/index.js
+++ server/index.js
@@ -10,13 +10,18 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
 
     const server = express();
+    // we are handling all of the request coming to our server
     server.get('*', (req, res) => {
+        // next.js is handling requests and providing the pages we are navigating to
+        // return res.json({message: 'Welcome to the server'});
         return handle(req, res);
     });
 
     const PORT = process.env.PORT || 3000;
 
-    server.use(handle).listen(PORT, (err) => {
+    // This also handled all of the request coming to our server
+    // server.use(handle).listen(PORT, (err) => {
+    server.listen(PORT, (err) => {
         if (err) throw err;
         console.log('> Ready on port ' + PORT);
     });
